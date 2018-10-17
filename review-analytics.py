1654e5e785783ccf9865c4038f04186b417f849b
Scott Zhang <zmtyoso@gmail.com>
2018-10-16 23:56:56 -0400

    calc a length of reviews

--- review-analytics.py
+++ review-analytics.py
@@ -6,8 +6,10 @@ with open('reviews.txt', 'r') as f:
 		count += 1 # count = count + 1
 		if count % 10000 == 0:
 			print(len(data))
-print(len(data))
+print('finish reading, total review number is', len(data))
 
-print(data[0])
-print('--------')
-print(data[1])
+sum_len = 0
+for d in data:
+	sum_len += len(d) 
+
+print ('average is', sum_len/len(data))
